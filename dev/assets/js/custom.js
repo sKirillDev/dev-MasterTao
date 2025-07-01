7ddf40e91b203b50f06b5b817a97fef32060defe
sKirillDev <k.sychov.dp@gmail.com>
2025-07-01 19:56:01 +0300

    feat: added swiper

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,2 +1,20 @@
 const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    clickable: true,
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
