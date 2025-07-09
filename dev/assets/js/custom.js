3ea27ceb00a97e5d58cb8dc963b3e30d43fe24af
sKirillDev <k.sychov.dp@gmail.com>
2025-07-09 15:55:34 +0300

    feat: creat first-screen height using js

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -17,15 +17,12 @@ const swiperBanner = new Swiper('.swiper-banner', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    992: {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-    },
   }
 });
+
+const header = document.querySelector('header');
+const banner = document.querySelector('.swiper-banner');
+const headerHeight = header.offsetHeight;
+
+banner.setAttribute('style', '--header-height:'+ headerHeight + 'px' );
+console.log(header.offsetHeight);
