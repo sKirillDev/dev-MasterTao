efce27da2bf03795ed200821e5e9ce48da65a8ab
sKirillDev <k.sychov.dp@gmail.com>
2025-07-25 18:10:16 +0300

    feat: refactoring script for input[type='file']

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -26,3 +26,22 @@ const headerHeight = header.offsetHeight;
 
 banner.setAttribute('style', '--header-height:'+ headerHeight + 'px' );
 console.log(header.offsetHeight);
+
+
+
+const fileInput = document.querySelector('#calculation-file');
+const fileName = document.querySelector('#file-name')
+
+fileInput.addEventListener('change', () => {
+   fileName.innerHTML = fileInput.files[0].name;
+});
+
+
+const fileInput = document.querySelectorAll('[type="file"]');
+
+fileInput.forEach(item => {
+    item.addEventListener('change', function() {
+        let fileName = item.closest('.input-box').querySelector('.file-name')
+        fileName.innerHTML = item.files[0].name;
+    })
+})
