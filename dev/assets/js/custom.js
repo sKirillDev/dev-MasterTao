9985deaf1923045545cf90c991e0a5dc3b2f326e
sKirillDev <k.sychov.dp@gmail.com>
2025-08-03 22:47:13 +0300

    feat: added bg-accent overlay

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -6,32 +6,32 @@ searchboxRemove.forEach(el => el.remove())
 
 
 const btnBurger = document.querySelector('.hamburger')
-btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
+btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active'))
 
 const swiperBanner = new Swiper('.swiper-banner', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 24,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 24,
 
-  // If we need pagination
-  pagination: {
-    clickable: true,
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 });
 
 const header = document.querySelector('header');
 const banner = document.querySelector('.swiper-banner');
 const headerHeight = header.offsetHeight;
 
-banner.setAttribute('style', '--header-height:'+ headerHeight + 'px' );
+banner.setAttribute('style', '--header-height:' + headerHeight + 'px');
 console.log(header.offsetHeight);
 
 const fileInput = document.querySelectorAll('[type="file"]');
@@ -42,3 +42,32 @@ fileInput.forEach(item => {
         fileName.innerHTML = item.files[0].name;
     })
 })
+
+
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
+
+const swiperAdvantages = new Swiper('#advantages', {
+    spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    roundLengths: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        992: {
+            spaceBetween: 0,
+            slidesPerView: advantagesSlideLength,
+            slidesPerGroup: advantagesSlideLength,
+            loop: false,
+            simulateTouch: false,
+            pagination: {
+                enabled: false,
+                el: '.swiper-pagination',
+            }
+        }
+    }
+});
